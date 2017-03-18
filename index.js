39ad380a088d0fbbe2ff69817805ad74d6529c33
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-03-18 15:24:42 +0700

    Add logs

--- index.js
+++ index.js
@@ -22,12 +22,14 @@ var port = config.get('port');
 function handlerError(res, error) {
     //var errorCode = error.code;
     //var errorMessage = error.message;
+    console.error(error);
     res.status(500).send(error);
 }
 
 app.post("/createUserWithEmailAndPassword", function(req, res) {
     var email = req.body.email;
     var password = req.body.password;
+    console.log('createUserWithEmailAndPassword: ' + JSON.stringify(req.body));
     firebase.auth().createUserWithEmailAndPassword(email, password).then(result => {
         res.status(200).send(result);
     }).catch(error => handlerError(res, error));
@@ -36,6 +38,7 @@ app.post("/createUserWithEmailAndPassword", function(req, res) {
 app.post("/signInWithEmailAndPassword", function(req, res) {
     var email = req.body.email;
     var password = req.body.password;
+    console.log('signInWithEmailAndPassword: ' + JSON.stringify(req.body));
     firebase.auth().signInWithEmailAndPassword(email, password).then(result => {
         res.status(200).send(result);
     }).catch(error => handlerError(res, error));
@@ -43,6 +46,7 @@ app.post("/signInWithEmailAndPassword", function(req, res) {
 
 app.post("/signInWithFacebook", function(req, res) {
     var access_token = req.body.access_token;
+    console.log('signInWithFacebook: ' + JSON.stringify(req.body));
     var credential = firebase.auth.FacebookAuthProvider.credential(access_token);
     firebase.auth().signInWithCredential(credential).then(result => {
         res.status(200).send(result);
@@ -51,6 +55,7 @@ app.post("/signInWithFacebook", function(req, res) {
 
 app.post("/signInWithGithub", function(req, res) {
     var access_token = req.body.access_token;
+    console.log('signInWithGithub: ' + JSON.stringify(req.body));
     var credential = firebase.auth.GithubAuthProvider.credential(access_token);
     firebase.auth().signInWithCredential(credential).then(result => {
         res.status(200).send(result);
@@ -60,6 +65,7 @@ app.post("/signInWithGithub", function(req, res) {
 app.post("/signInWithGoogle", function(req, res) {
     var id_token = req.body.id_token;
     var access_token = req.body.access_token;
+    console.log('signInWithGoogle: ' + JSON.stringify(req.body));
     var credential = firebase.auth.GoogleAuthProvider.credential(id_token, access_token);
     firebase.auth().signInWithCredential(credential).then(result => {
         res.status(200).send(result);
@@ -69,6 +75,7 @@ app.post("/signInWithGoogle", function(req, res) {
 app.post("/signInWithTwitter", function(req, res) {
     var token = req.body.token;
     var secret = req.body.secret;
+    console.log('signInWithTwitter: ' + JSON.stringify(req.body));
     var credential = firebase.auth.TwitterAuthProvider.credential(token, secret);
     firebase.auth().signInWithCredential(credential).then(result => {
         res.status(200).send(result);
@@ -76,6 +83,7 @@ app.post("/signInWithTwitter", function(req, res) {
 });
 
 app.post("/signInAnonymously", function(req, res) {
+    console.log('signInAnonymously: ' + JSON.stringify(req.body));
     firebase.auth().signInAnonymously().then(result => {
         res.status(200).send(result);
     }).catch(error => handlerError(res, error));
